1822118b70942fd952c1cc4b9bfc8e4c3a182a4e
Benedikt Schwering <mail@bschwer.ing>
2024-09-24 20:28:56 +0200

    feat(api): add upstream api parser

--- api/src/models.py
+++ api/src/models.py
@@ -17,7 +17,7 @@ class Verse(BaseModel):
     sheet: Optional[str] = None
 
 class Section(BaseModel):
-    title: str
+    title: Optional[str] = None
     verses: list[Verse]
 
 class PrayerType(Enum):
@@ -37,20 +37,84 @@ class Prayer(BaseModel):
     name: str
     type: PrayerType
     sections: list[Section]
-    
+
     @field_validator('sections', mode='before')
     def parse_sections(raw: str) -> list[Section]:
-        html = BeautifulSoup(raw)
+        __verse_type = {
+            'format_none': 'generic',
+            'format_removeFormat': 'generic',
+            'format_subhbold': 'comment',
+            'format_subh': 'comment',
+            'format_center': 'comment',
+            'format_bold': 'subtitle',
+            'format_preAnt': 'response',
+            'format_preFirstAnt': 'response',
+            'format_preSecAnt': 'response',
+            'format_preThiAnt': 'response',
+            'format_preOra': 'oration',
+            'format_preAntiphon': 'antiphon',
+        }
 
-        print(html.children)
+        __verse_ordination = {
+            'format_preFirstAnt': 1,
+            'format_preSecAnt': 2,
+            'format_preThiAnt': 3,
+        }
 
-        return [
-            Section(
-                title=html.prettify(),
-                subtitle='',
-                verses=[],
-            )
-        ]
+        html = BeautifulSoup(
+            markup=raw,
+            features='html.parser'
+        )
+
+        sections = []
+        current_section = Section(
+            title=None,
+            verses=[],
+        )
+
+        for element in html.children:
+            if element.name:
+                match element.name:
+                    case 'h2':
+                        if element.text.strip().upper() == element.text.strip():
+                            sections.append(current_section)
+
+                            current_section = Section(
+                                title=element.text.strip(),
+                                verses=[],
+                            )
+                        else:
+                            current_section.verses.append(
+                                Verse(
+                                    type=VerseType.subtitle,
+                                    ordination=None,
+                                    text=element.text.strip(),
+                                )
+                            )
+                    case 'p':
+                        spans = element.find_all(
+                            name='span',
+                            attrs={
+                                'class': 'hl',
+                            },
+                        )
+
+                        for text in [
+                            element.text.strip()
+                        ] if len(spans) == 0 else [
+                            span.text.strip()
+                                for span in spans if span.text.strip() != ''
+                        ]:
+                            current_section.verses.append(
+                                Verse(
+                                    type=__verse_type.get(element['class'][0], VerseType.generic),
+                                    ordination=__verse_ordination.get(element['class'][0], None),
+                                    text=text,
+                                )
+                            )
+
+        sections.append(current_section)
+        return sections
 
 class DayOfPrayer(BaseModel):
     date: str = Field(validation_alias='datum')
